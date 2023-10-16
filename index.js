f3ee4360f724b8da83834902ff1cc28f64757334
daredaryl96 <113288805+daredaryl96@users.noreply.github.com>
2023-10-16 15:06:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/public'));
-app.use(cors()); // to find out why cors and cookieParser affect the authentication flow
+app.use(cors()); 
 app.use(cookieParser());
 app.use(session({
   secret: 'your-secret-key',
